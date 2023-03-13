e6656567958e04be4fc103adad638a04cc436714
Ivan Kravchenko <kravchenkovano04@gmail.com>
2023-03-13 16:24:56 +0200

    update

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ContactList = ({ children }) => {
+    return (
+        <div>
+            <h2>Contacts</h2>
+            {children}
+        </div>
+    )
+}
+
+
+
